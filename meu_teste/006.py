519dd365fbd9c499401d2a6a7278b4109df84a4e
codingscode <heronnestg@hotmail.com>
2021-09-13 12:34:17 -0300

    parte 6

---
+++ meu_teste/006.py
@@ -0,0 +1,19 @@
+
+arquivo_escrita = open('006_arquivo.txt', 'w')
+
+print('--------- inicio ----------')
+
+arquivo_escrita.write('conteudo da primeira linha.')
+arquivo_escrita.write('\nconteudo segunda linha.')
+arquivo_escrita.close()
+
+
+print('---------- fim ------------')
+# conteudo escrito
+
+"""
+--------- inicio ----------
+---------- fim ------------
+
+"""
+
