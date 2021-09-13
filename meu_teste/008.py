9d38ced4b9bddf6d71a4b2e3d760d65f083f92b3
codingscode <heronnestg@hotmail.com>
2021-09-13 19:16:20 -0300

    parte 8

---
+++ meu_teste/008.py
@@ -0,0 +1,14 @@
+arquivo_escrita = open('008_texto.txt', 'a')
+
+print('------- inicio --------')
+arquivo_escrita.write('\nconteudo adicional.')
+
+arquivo_escrita.close()
+
+print('------- fim --------')
+
+"""
+------- inicio --------
+------- fim --------
+
+"""
