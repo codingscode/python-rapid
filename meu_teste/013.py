363d58ba260ad7d6f952c950f459fbe31fb441ba
codingscode <heronnestg@hotmail.com>
2021-09-14 10:36:08 -0300

    parte 13

---
+++ meu_teste/013.py
@@ -0,0 +1,23 @@
+
+print('------------ inicio -------------')
+
+frase1 = 'a ida foi uma boa partida, a vida veio seguida.'
+lista_termos1 = frase1.split()
+print(lista_termos1)
+
+frase2 = 'amora  abacaxi   abacate    banana'
+lista_termos2 = frase2.split()
+print(lista_termos2)
+
+frase3 = 'carro,moto,avião'
+lista_termos3 = frase3.split(',')
+print(lista_termos3)
+print('-------------- fim -------------')
+
+"""
+------------ inicio -------------
+['a', 'ida', 'foi', 'uma', 'boa', 'partida,', 'a', 'vida', 'veio', 'seguida.']
+['amora', 'abacaxi', 'abacate', 'banana']
+['carro', 'moto', 'avião']
+-------------- fim -------------
+"""
