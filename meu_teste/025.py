244bde0c53dd3e3b5174c6d4198040632ec91173
codingscode <heronnestg@hotmail.com>
2021-09-17 19:04:31 -0300

    ->

--- meu_teste/025.py
+++ meu_teste/025.py
@@ -1,6 +1,22 @@
+import sqlite3 as conector
 
 
+# abertura de conexão
+conexao = conector.connect('URL SQLite')
 
+# aquisição de um cursor
+cursor = conexao.cursor()
+
+# execução comandos: SELECT..CREATE
+cursor.execute('...')
+cursor.fetchall()
+
+# efetivação do comando
+conexao.commit()
+
+# fechamento das conexoes
+cursor.close()
+conexao.close()
 
 
 
