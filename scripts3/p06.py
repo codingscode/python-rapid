21421abbdd673e52c4074836b9bca0a25096c3f8
codingscode <heronnestg@hotmail.com>
2021-10-30 19:54:31 -0300

    select-11

--- scripts3/p06.py
+++ scripts3/p06.py
@@ -12,3 +12,4 @@ tk.Radiobutton(janela, text="Python", padx = 25, variable=v, value=1).pack(ancho
 tk.Radiobutton(janela, text="C++", padx = 25, variable=v, value=2).pack(anchor=tk.W)
 
 janela.mainloop()
+
